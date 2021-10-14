cd65ec2912e92222d3b7bd0a4960cc33795ed9a2
mizushima-08 <goomailzusshi@gmail.com>
2021-10-14 16:25:22 +0000

    make comment

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4ee045af2c03f463de5fd54ae5cc3ebe867b28525b3bd9a71bccbdfd68739531cfc58c7791d23b082eaecb27c128ebf54fac9e3ad910a5965c46ee24ea81983a'
